a0f4175172ec70e2a138d51a412ca0ab21dd006a
Christian Ohanaja <39662993+ChrisOh431@users.noreply.github.com>
2025-01-12 16:32:16 -0500

    fixed up navbar, added footer, corrected fonts

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export function Navbar() {
+    const [isOpen, setOpen] = useState(false);
+
+    return (
+        <div className="navbar">
+
+            
+        </div>
+    )
+}
