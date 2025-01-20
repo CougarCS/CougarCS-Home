20b1d204c045800fa377b8363e4fadf17cff52df
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2025-01-19 19:40:25 -0600

    part 2/3, officer update
    adds combined contact us/officer listing page
    contact form uses the same sendgrid infra from original cougarcs.com
    
    adds officers callout at bottom of landing
    
    adds josh corneau CSS reset for easier development
    tweaks landing page to correct style diffs after css reset
    
    updated navbar links
    
    mobile tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     site: 'https://dev.cougarcs.com/',
+    output: 'server',
     integrations: [sitemap(), icon(), react()]
 });
